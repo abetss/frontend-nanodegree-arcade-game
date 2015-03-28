7a2a629ed303d9baee83ca3d61ee3617c4966f40
Antar Avika <abtin.ghods@gmail.com>
2015-03-28 17:35:24 -0400

    add player class, so the game can run without the initial errors

--- js/app.js
+++ js/app.js
@@ -1,25 +1,4 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-}
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -29,6 +8,14 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+
+
+var enemy1 = new Enemy();
+var allEnemies = []; 
+allEnemies.push(enemy1);
+
+var player = new Player();
 
 
 
---
+++ js/models/player.js
@@ -0,0 +1,17 @@
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function(dt) {
+    
+}
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
